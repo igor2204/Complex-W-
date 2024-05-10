8633b93a5e78293f785dccf57b0b42832b074517
Цомкалов Игорь <igorcomkalov@gmail.com>
2024-05-10 23:57:48 +0300

    all

--- Complex(W)/Program.cs
+++ Complex(W)/Program.cs
@@ -9,33 +9,28 @@
         Imaginary = imaginary;
     }
 
-    // Сложение
     public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
     {
         return new ComplexNumber(a.Real + b.Real, a.Imaginary + b.Imaginary);
     }
 
-    // Вычитание
     public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
     {
         return new ComplexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
     }
 
-    // Умножение
     public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
     {
         return new ComplexNumber(a.Real * b.Real - a.Imaginary * b.Imaginary,
                                 a.Real * b.Imaginary + a.Imaginary * b.Real);
     }
 
-    // Деление
     public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
     {
         double denominator = b.Real * b.Real + b.Imaginary * b.Imaginary;
         return new ComplexNumber((a.Real * b.Real + a.Imaginary * b.Imaginary) / denominator,
                                 (a.Imaginary * b.Real - a.Real * b.Imaginary) / denominator);
     }
-    // Переопределение ToString для вывода
     public override string ToString()
     {
         return $"{Real} + {Imaginary}i";
